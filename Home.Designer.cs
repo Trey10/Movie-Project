a1f427f1f312c64b7b7b64221df810dfcf0b3a88
Trey10 <tdw53@zips.uakron.edu>
2017-03-08 21:35:10 -0500

    Movie Project
    Initial commit, started design of home screen. Included, is a picture of
    the home screen.

---
+++ Home.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace WindowsFormsApplication9
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Category = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Rating = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.discontinuedDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 118);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Movie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 70);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Home";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 468);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Check In";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(124, 468);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 28);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Check Out";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(531, 48);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Refresh";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(497, 468);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 471);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Quantity Update";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Title,
+            this.Category,
+            this.Rating,
+            this.Quantity,
+            this.discontinuedDate});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(5, 154);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(636, 306);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Title
+            // 
+            this.Title.Text = "Movie Title";
+            this.Title.Width = 174;
+            // 
+            // Category
+            // 
+            this.Category.Text = "Category";
+            this.Category.Width = 75;
+            // 
+            // Rating
+            // 
+            this.Rating.Text = "Rating";
+            this.Rating.Width = 65;
+            // 
+            // Quantity
+            // 
+            this.Quantity.Text = "Quantity";
+            this.Quantity.Width = 59;
+            // 
+            // discontinuedDate
+            // 
+            this.discontinuedDate.Text = "Discontinued Date";
+            this.discontinuedDate.Width = 103;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(457, 84);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 28);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Discontinued/Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(513, 500);
+            this.Update.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(100, 28);
+            this.Update.TabIndex = 12;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Select Rating",
+            "G",
+            "PG",
+            "PG-13",
+            "R",
+            "NC-17"});
+            this.comboBox3.Location = new System.Drawing.Point(16, 70);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(177, 24);
+            this.comboBox3.TabIndex = 18;
+            this.comboBox3.Text = "Select Rating";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Select Category",
+            "Comedy",
+            "Drama",
+            "Horror",
+            "Adventure",
+            "Action",
+            "Sci-Fi",
+            "Western"});
+            this.comboBox2.Location = new System.Drawing.Point(16, 108);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(179, 24);
+            this.comboBox2.TabIndex = 19;
+            this.comboBox2.Text = "Select Category";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 118);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 28);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(36, 500);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 21);
+            this.checkBox1.TabIndex = 21;
+            this.checkBox1.Text = "View Discountinued";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(503, 118);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(128, 28);
+            this.button7.TabIndex = 22;
+            this.button7.Text = "Undo Delete";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(647, 537);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.Shown += new System.EventHandler(this.Home_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader Category;
+        private System.Windows.Forms.ColumnHeader Rating;
+        private System.Windows.Forms.ColumnHeader Quantity;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        public System.Windows.Forms.ListView listView1;
+        public System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        public System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader discontinuedDate;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Home.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+
+    public partial class Home : Form
+    {
+        
+
+        public Home()
+        {
+           
+
+
+
+        }
+
+
+
+        public void fillList()
+        {
+
+            
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Shown(object sender, EventArgs e)
+        { }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+          
+          
+        }
+
+        public void button4_Click(object sender, EventArgs e)
+        {
+
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        public void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string rating = this.comboBox3.GetItemText(this.comboBox3.SelectedItem);
+            string category = this.comboBox2.GetItemText(this.comboBox2.SelectedItem);
+
+
+
+
+
+            if ((rating != "Select Rating"))
+            {
+                listView1.Items.Clear();
+                sortMovies();
+            }
+            else if ((category == "Select Category") && (rating == "Select Rating"))
+            {
+                fillList();
+            }
+
+            else if ((category != "Select Category") && (rating == "Select Rating"))
+            {
+                categorySortMovies();
+            }
+
+            else if ((comboBox2.SelectedItem == "Select Category") && (rating != "Select Rating"))
+            {
+                comboBox3.Select();
+                rateSortMovies();
+            }
+
+            else
+            {
+                listView1.Items.Clear();
+                sortMovies();
+            }
+
+
+        }
+
+        private void reset()
+        {
+
+        }
+
+        public void button2_Click_1(object sender, EventArgs e)
+        {
+            Form1 add = new Form1();
+            Home close = new Home();
+
+            if (listView1.SelectedIndices.Count < 1)
+            {
+                MessageBox.Show("Please Select a Movie");
+            }
+
+            else
+            {
+                add.Show();
+                string title = listView1.SelectedItems[0].Text;
+                string rating;
+                add.textBox1.Text = title;
+                add.comboBox1.FindString(listView1.SelectedItems[0].SubItems[2].Text).ToString();
+                add.comboBox2.FindString(listView1.SelectedItems[0].SubItems[1].Text);
+                add.textBox2.Text = listView1.SelectedItems[0].SubItems[3].Text;
+                string finaltitle = listView1.SelectedItems[0].Text;
+                
+
+            }
+        }
+     
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            fillList();
+            
+        }
+
+      
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form1 add = new Form1();
+            if (listView1.SelectedIndices.Count < 1)
+            {
+                MessageBox.Show("Please select a movie");
+            }
+            else
+            {
+
+
+                SqlCommand cmd = new SqlCommand("UPDATE [Movie Project].[dbo].[movie_id] SET [Title] = @title,[categoryLookup_id] = @category,[ratingLookup_id] = @rating,[Quantity] = @quantity,[discontinueDate] = null WHERE Title = @title", con);
+
+                cmd.Parameters.AddWithValue("@title", listView1.SelectedItems[0].Text);
+                cmd.Parameters.AddWithValue("@category", listView1.SelectedItems[0].SubItems[1].Text);
+                cmd.Parameters.AddWithValue("@rating", listView1.SelectedItems[0].SubItems[2].Text);
+                cmd.Parameters.AddWithValue("@quantity", listView1.SelectedItems[0].SubItems[3].Text);
+                cmd.Parameters.AddWithValue("null", listView1.SelectedItems[0].SubItems[4].ToString());
+
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    fillList();
+                }
+
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}  
+
+        
+
+    
+
+ 
